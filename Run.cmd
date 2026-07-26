d1830101288f2e66b32a4b29ed72b2f144f65a7b
Kevin Vu <kevin.m.vu@gmail.com>
2026-07-26 09:07:27 -0700

    Fix paths

--- Run.cmd
+++ Run.cmd
@@ -1,1 +1,1 @@
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\kevin\Repository\Godot\VsyncStutterTest\Run.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%UserProfile%\Repository\Godot\VsyncStutterTest\Run.ps1"
--- Run.ps1
+++ Run.ps1
@@ -1,19 +1,20 @@
 Set-StrictMode -Version "Latest"
 $ErrorActionPreference = "Stop"
 
-$logFolderPath = "C:\Users\kevin\Repository\Godot\VsyncStutterTest\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
+$logFolderPath = "$env:UserProfile\Repository\Godot\VsyncStutterTest\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
 New-Item -ItemType "Directory" -Path $logFolderPath
 
-$presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
-Start-Process `
-  -FilePath "C:\Users\kevin\Program\PresentMon-2.5.1-x64.exe" `
-  -ArgumentList "--process_name `"VsyncStutterTest.exe`" --output_file `"$($presentMonLogFilePath)`"" `
-  -Verb "RunAs"
-
-$process = `
+$presentMonPath = "$env:UserProfile\Program\PresentMon-2.5.1-x64.exe"
+if (Test-Path $presentMonPath) {
+    $presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
     Start-Process `
-        -FilePath "C:\Users\kevin\Repository\Godot\VsyncStutterTest\MyBuildOutput\VsyncStutterTest.exe" `
-        -PassThru
+    -FilePath $presentMonPath `
+    -ArgumentList "--process_name `"VsyncStutterTest.exe`" --output_file `"$($presentMonLogFilePath)`"" `
+    -Verb "RunAs"
+}
+
+$process = Start-Process -FilePath "$env:UserProfile\Repository\Godot\VsyncStutterTest\MyBuildOutput\VsyncStutterTest.exe" -PassThru
+# -ArgumentList "--screen 1"
 
 $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
 Write-Host "Launched with PID=$($process.Id)"
