e7c654152005aae8ee70ce2593fe88be17cc2d39
Kevin Vu <kevin.m.vu@gmail.com>
2026-08-28 20:37:48 -0700

    Disable presentmon by default

--- Run.ps1
+++ Run.ps1
@@ -4,14 +4,14 @@ $ErrorActionPreference = "Stop"
 $logFolderPath = "$env:UserProfile\Repository\Godot\VsyncStutterTest\MyLogOutput\$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss")"
 New-Item -ItemType "Directory" -Path $logFolderPath
 
-$presentMonPath = "$env:UserProfile\Program\PresentMon-2.5.1-x64.exe"
-if (Test-Path $presentMonPath) {
-    $presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
-    Start-Process `
-        -FilePath $presentMonPath `
-        -ArgumentList "--process_name `"VsyncStutterTest.exe`" --output_file `"$($presentMonLogFilePath)`"" `
-        -Verb "RunAs"
-}
+# $presentMonPath = "$env:UserProfile\Program\PresentMon-2.5.1-x64.exe"
+# if (Test-Path $presentMonPath) {
+#     $presentMonLogFilePath  = "$($logFolderPath)\PresentMon.csv"
+#     Start-Process `
+#         -FilePath $presentMonPath `
+#         -ArgumentList "--process_name `"VsyncStutterTest.exe`" --output_file `"$($presentMonLogFilePath)`"" `
+#         -Verb "RunAs"
+# }
 
 $process = Start-Process -FilePath "$env:UserProfile\Repository\Godot\VsyncStutterTest\MyBuildOutput\VsyncStutterTest.exe" -PassThru
 
